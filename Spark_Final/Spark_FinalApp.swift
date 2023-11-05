a2c0f93f3df1e38456d2689839f391cfa4bc2629
Avihhan <avihanarya111@gmail.com>
2023-11-05 02:31:44 -0500

    Initial Commit

---
+++ Spark_Final/Spark_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spark_FinalApp.swift
+//  Spark_Final
+//
+//  Created by Avihhan Arya Kumarr on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Spark_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
